d508bf857da5d833b035f928c4bc141b30628fd1
VladisLuv <mukpo1kg@gmail.com>
2020-08-10 10:41:31 +0300

    randomChar block updated

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './spinner.css';
+
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-6p3co5hgcpw">
+            <div className="ldio-k3fi1pvd9z">
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+                <div>
+                    <div/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
